79a96e14a7efc4fe98e47b9f0d15f32e117cf34c
muv11 <uvmozgovaya@proton.me>
2022-10-11 19:17:55 +0500

    create Spring context and check its work

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,9 +1,22 @@
 package main;
 
+import configs.ProjectConfiguration;
+import models.PersonFromBook;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import services.AddingNewPersonsService;
+
 public class Main {
 
     public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(ProjectConfiguration.class);
 
+        PersonFromBook person = new PersonFromBook();
+
+        AddingNewPersonsService addingService;
+
+        addingService = context.getBean(AddingNewPersonsService.class);
+        addingService.addNewPersonToBook(person);
     }
 
 }
