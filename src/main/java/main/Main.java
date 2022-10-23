717ed2a5473148ae3fd5dc2cac9e33201a3b2d33
muv11 <uvmozgovaya@proton.me>
2022-10-23 17:29:03 +0500

    add @SpringBootApplication to Main.java

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,12 +2,18 @@ package main;
 
 import configs.ProjectConfiguration;
 import models.PersonFromBook;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import services.AddingNewPersonsService;
 
+@SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
+
+        SpringApplication.run(main.Main.class, args);
+
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(ProjectConfiguration.class);
 
